9284ef047a2f897bd077ebb4e0c8121afcf89fdf
Yaxar Maxson <yaxar.murat@gmail.com>
2011-12-12 10:04:02 -0800

    Added some comments

--- log.pl
+++ log.pl
@@ -1,30 +1,40 @@
 #!/usr/bin/env perl
+#Now we are going to load all of what it needs.
 use Mojolicious::Lite;
 use EV;
 use AnyEvent::IRC;
 use AnyEvent::IRC::Client;
 use DateTime;
-# Join #mojo on irc.perl.org
+#Below we will create a new AnyEvent::IRC instance
 my $irc = AnyEvent::IRC::Client->new;
+#Here we mark the default values
 my $channel = "#mojo";
 my $nick = "guest$$";
 my $user = "guest$$";
 my $real = "guest$$";
 my $test = $channel;
 my $server = 'irc.perl.org';
+#This is the / funciton :)
 get '/' => sub {
 	my $self = shift;
+	#making sure that we will connect with the latest info
 	$nick = $self->param('nick') || $nick;
 	$channel = $self->param('channel') || $channel;
+	#sending the data to the template
 	$self->stash(nick => $nick);
+	#rendering the template
 	$self->render('index', channel =>$channel);
+	#changing our nick
 	$irc->send_srv(NICK => $nick);
+	#calling the change channel function
 	channelr($channel);
 };
+#connecting to the irc server
 $irc->connect($server, 6667, {nick => $nick, user => $user, real => $real});
 
 sub channelr {
 	my ($channel)  = @_;
+	#here we check if the $channel is the same as $test or not
 	if ($channel ne $test) {
 		$irc->send_srv(JOIN => $channel);
 		$irc->send_srv(PART => $test);
@@ -32,6 +42,7 @@ sub channelr {
 		$irc->send_srv(JOIN => $channel);
 	}
 }
+#here we update the log file
 sub updatefile {
 	my ($channel, $message)  = @_;
 	my $dt = DateTime->now;
@@ -45,6 +56,7 @@ sub updatefile {
 		or die "cannot open append output.txt: $!";
 	print $n "$message \n";
 }
+#this is where all the logic comes in :)
 get '/events' => sub {
 	my $self = shift;
 	Mojo::IOLoop->stream($self->tx->connection)->timeout(300000000);
@@ -61,24 +73,28 @@ get '/events' => sub {
 		my $result = updatefile($channel, $message);
 		$self->write("event:msg\ndata: $message\n\n");
 	});
+	# Emit "join" event for every new IRC join
 	my $j = $irc->reg_cb(join => sub {
 		my ($ls, $nick, $channel, $is_myself) = @_;
 		my $message = "$nick joined the chat room.\n";
 		my $result = updatefile($channel, $message);
 		$self->write("event:msg\ndata: $message\n\n");
 	});
+	# Emit "nick_change" event for every new IRC nick_change
 	my $nc = $irc->reg_cb(nick_change => sub {
 		my ($ls, $ld_nick, $new_nick, $is_myself)  = @_;
 		my $message = "$ld_nick changed his/her nickname to $new_nick.\n";
 		my $result = updatefile($channel, $message);
 		$self->write("event:msg\ndata: $message\n\n");
 	});
+	# Emit "quit" event for every new IRC quit
 	my $qu = $irc->reg_cb(quit => sub {
 		my ($sl, $nick, $msg)  = @_;
 		my $message = "$nick left the chat room.\n";
 		my $result = updatefile($channel, $message);
 		$self->write("event:msg\ndata: $message\n\n");
 	});
+	# Here we finish it all of and unreg everything
 	$self->on(finish => sub { 
 		$irc−>unreg_cb($pm);
 		$irc−>unreg_cb($j);
